80a29fa23891122ada3381d076ff982c97678c6a
Sam Roxanne <lunar_sam@protonmail.com>
2021-05-19 02:44:32 -0500

    make noimg work

--- api/files.lua
+++ api/files.lua
@@ -2,12 +2,13 @@ local api = ...
 local blake2b = require("utils.blake2b")
 local models = require("models")
 local files = require("utils.files")
+local cfg = require("utils.luna-config")
 
 function api.upload_picture(args)
 	local fdat = args.picture.content
 	local fname = args.picture.filename
 	local mime = args.picture["content-type"]
-	if not fdat or #fdat == 0 then args.picture = nil return end
+	if not fdat or #fdat == 0 or cfg.boardinfo[args.board].noimg then args.picture = nil return end
 	for k, v in pairs(args.picture) do
 		print(k)
 	end
